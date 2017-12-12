119dfdcbf2670f18262cff3e81d3c8458db3589b
TurboHz <turbohz@gmail.com>
2017-12-12 14:19:12 +0100

    Adds day01 solution

---
+++ day01.fsx
@@ -0,0 +1,23 @@
+open System
+
+let firstValueIfEqualToLast l =
+    let first = l |> List.head
+    let last = l |> List.rev |> List.head
+    if first = last then first else 0
+
+let rec collectSumOfEqualPairs s l =
+    match l with
+    | [] -> 0
+    | [_] -> s
+    | x1::x2::xs -> 
+        let v = if x1=x2 then x1 else 0
+        collectSumOfEqualPairs (s+v) (x2::xs)
+
+let input = Console.ReadLine() |> List.ofSeq |> List.map (int >> (+) -48)
+
+// We apply a divide an conquer approach:
+// - collect the sum of equal pairs
+// - add to that the first value if equal to the last value
+let output = (collectSumOfEqualPairs 0 input) + (firstValueIfEqualToLast input)
+printfn "%d" output
+exit 0
