10507015575c8b3d9d09c91bba557329438dd50a
TurboHz <turbohz@gmail.com>
2017-12-21 21:59:15 +0100

    Improves day02

--- day02.fsx
+++ day02.fsx
@@ -1,23 +1,14 @@
-open System
+#load "util.fs"
 
+open System
+open Util.Stdin
 let parsed c =
     match Int32.TryParse (string c) with
     | (true , i) -> Some i
     | (false, _) -> None
-
-let (|Line|_|) (r:IO.TextReader) = if r.Peek() = -1 then None else Some (r.ReadLine())
-let rec readLines (reader:IO.TextReader) (lines: string list) : string list = 
-    match reader with
-    | Line l -> readLines reader (lines @ [l])
-    | _ -> lines
-
-let computeLineChecksum cells = (List.max cells - List.min cells)
-
-let input = Console.OpenStandardInput()
-let reader = new IO.StreamReader(input)
-// create a list of lists of ints
-let cells = [] |> readLines reader |> List.map (fun l -> l.Split('\t') |> List.ofSeq |> List.choose parsed)
-let output = cells |> List.sumBy computeLineChecksum
+let computeLineChecksum cells = (Seq.max cells - Seq.min cells)
+let cells = Util.Stdin.lines |> Seq.map (fun l -> l.Split('\t') |> Seq.choose parsed)
+let output = cells |> Seq.sumBy computeLineChecksum
 
 printfn "%d" output
 exit 0
--- day02b.fsx
+++ day02b.fsx
@@ -1,19 +1,12 @@
-open System
-open System.Xml.Xsl
+#load "util.fs"
 
+open System
+open Util.Stdin
 let parsed c =
     match Int32.TryParse (string c) with
     | (true , i) -> Some i
     | (false, _) -> None
-
-let (|Line|_|) (r:IO.TextReader) = if r.Peek() = -1 then None else Some (r.ReadLine())
-let rec readLines (reader:IO.TextReader) (lines: string list) : string list = 
-    match reader with
-    | Line l -> readLines reader (lines @ [l])
-    | _ -> lines
-
 let evenlyDividable x1 x2 = (x1 % x2 = 0)
-
 let rec computeLineChecksum =
     function
     | [] -> failwith "Did not find two evenly divisible elements. Doh!"
@@ -23,11 +16,8 @@ let rec computeLineChecksum =
         | Some v -> v
         | None -> computeLineChecksum xs
 
-let input = Console.OpenStandardInput()
-let reader = new IO.StreamReader(input)
-// create a list of lists of ints
-let cells = [] |> readLines reader |> List.map (fun l -> l.Split('\t') |> List.ofSeq |> List.choose parsed |> List.sortDescending)
-let output = cells |> List.sumBy computeLineChecksum
+let cells = Util.Stdin.lines |> Seq.map (fun l -> l.Split('\t') |> Seq.choose parsed |> Seq.sortDescending |> List.ofSeq)
+let output = cells |> Seq.sumBy computeLineChecksum
 
 printfn "%d" output
 exit 0
